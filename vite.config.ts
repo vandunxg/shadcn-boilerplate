ab5bbef6eb49e05226a83fd9807857b499bec593
vandunxg <nguyenvandung.official@gmail.com>
2025-10-21 16:28:26 +0700

    update alias

--- vite.config.ts
+++ vite.config.ts
@@ -1,20 +1,24 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
 import path from "path";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
-      "@components": path.resolve(__dirname, "./src/components"),
-      "@hooks": path.resolve(__dirname, "./src/hooks"),
-      "@pages": path.resolve(__dirname, "./src/pages"),
-      "@store": path.resolve(__dirname, "./src/store"),
-      "@types": path.resolve(__dirname, "./src/types"),
-      "@utils": path.resolve(__dirname, "./src/utils")
-    }
-  }
-})
+    plugins: [react(), tailwindcss()],
+    resolve: {
+        alias: {
+            "@": path.resolve(__dirname, "./src"),
+            "@components": path.resolve(__dirname, "./src/components"),
+            "@hooks": path.resolve(__dirname, "./src/hooks"),
+            "@pages": path.resolve(__dirname, "./src/pages"),
+            "@stores": path.resolve(__dirname, "./src/stores"),
+            "@types": path.resolve(__dirname, "./src/types"),
+            "@utils": path.resolve(__dirname, "./src/utils"),
+            "@services": path.resolve(__dirname, "./src/services"),
+            "@configs": path.resolve(__dirname, "./src/configs"),
+            "@routes": path.resolve(__dirname, "./src/routes"),
+            "@lib": path.resolve(__dirname, "./src/lib"),
+        },
+    },
+});
